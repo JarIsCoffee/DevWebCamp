0a4702d9d96ec3476d6d11247ce1130b40c0e4df
Jairo Joaquin <jairojoaquin25@gmail.com>
2025-10-30 21:52:34 -0600

    agregando la tabla de usuarios al script de la bd

--- DB_webConferencias/Scritpt_Creacion.sql
+++ DB_webConferencias/Scritpt_Creacion.sql
@@ -1,5 +1,21 @@
+#Creando base de datos 'DebWebCamp'
+CREATE DATABASE devwebcamp;
 
-create database devwebcamp;
+#Seleccionando la base de datos para operaciones
+USE devwebcamp;
 
+#Tabla de usuarios 
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) DEFAULT NULL,
+  `apellido` varchar(40) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `confirmado` tinyint(1) DEFAULT NULL,
+  `token` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-// Comentario de pedro 
+#comandos...
+SELECT * FROM usuarios;
